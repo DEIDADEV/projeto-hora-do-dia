b4c512f67de5d89a7c675ee37d22b67832dd6947
DEIDADEV <161657805+DEIDADEV@users.noreply.github.com>
2024-05-09 21:47:34 -0300

    alterado as imagens

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    //var hora = 4
+    //var hora = 22
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //BOM DIA
-        img.src = 'manha.jpg'
+        img.src = 'manha.png'
         document.body.style.background = '#bbae8d'
     } else if (hora >= 12 && hora <= 18) {
         //BOA TARDE
-        img.src = 'tarde.jpg'
+        img.src = 'tarde.png'
         document.body.style.background = '#a16a55'
     } else {
     //BOA NOITE
-    img.src = 'noite.jpg'
-    document.body.style.background = '#343530'
+    img.src = 'noite.png'
+    document.body.style.background = '#001110'
     }
 }
